d157094e8c76fee7af1a3fe5fcf779bcd9d96ba5
Martin Häger <martin.haeger@gmail.com>
2020-02-08 17:03:05 +0100

    chore(scripts): dependency update script

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -A excludes
+while getopts ":x:" opt; do
+	case $opt in
+	x) excludes[${OPTARG:-}]=x;;
+	\?) echo "invalid option: -${OPTARG:-}" >&2; exit 1;;
+	:) echo "option -${OPTARG:-} requires an argument" >&2; exit 1;;
+	esac
+done
+
+shift "$((OPTIND-1))"
+
+while IFS=: read -u 3 -r _ _ current latest type _; do
+	package=${current%@*}
+	if [[ -v excludes[$package] ]] || ! npx semver -r "<${latest##*@}" ${current##*@} >/dev/null; then
+		continue
+	fi
+
+	case $type in
+	devDependencies) commitType=chore; saveFlag=--save-dev;;
+	dependencies) commitType=fix; saveFlag=--save;;
+	*) continue;;
+	esac
+
+	npm install $saveFlag "$latest"
+	npm test
+	git add package{,-lock}.json
+	git commit -m "$commitType(package): update $package from ${current##*@} to ${latest##*@}"
+done 3< <(npm outdated --long --parseable)
