312dbdf6c58bf09d871ce096ab9855aea47606df
Martin Häger <martin.haeger@gmail.com>
2021-03-06 01:01:24 +0100

    fix(copyright): update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,29 +15,14 @@ const Footer = ({ ...props }) => (
 						repositoryUrl
 					}
 				}
-
-				latestPost: allMarkdownRemark(
-					sort: { fields: [frontmatter___date], order: DESC }
-					limit: 1
-				) {
-					edges {
-						node {
-							frontmatter {
-								date
-							}
-						}
-					}
-				}
 			}
 		`}
-		render={({ site, latestPost }) => {
+		render={({ site }) => {
 			const { author, repositoryUrl } = site.siteMetadata;
-			const latestPostYear = latestPost.edges.length > 0 && moment(latestPost.edges[0].node.frontmatter.date).year();
-			const copyrightRange = `2018${latestPostYear && latestPostYear > 2018 ? ` - ${latestPostYear}` : ''}`;
 
 			return (
 				<Segment basic {...props}>
-					<p>&copy; {author}, {copyrightRange}. Some rights reserved. The <a href={repositoryUrl}>
+					<p>&copy; {author}, 2018 - 2021. Some rights reserved. The <a href={repositoryUrl}>
 						site source</a> is available under the MIT license.</p>
 					<p>
 						v{version} (<a href={`${repositoryUrl}/blob/master/CHANGELOG.md`}>changelog</a>). Built with <a href="https://graphql.org/">GraphQL</a>
