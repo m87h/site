207354d3622bc896bed0732f110cfd81237b783d
Martin Häger <martin.haeger@gmail.com>
2022-06-09 23:23:09 +0200

    chore: reconfigure babel

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,20 @@ module.exports = {
 	moduleNameMapper: {
 		'.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
 		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/unit/__mocks__/fileMock.js',
+		'^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771
+    '^gatsby-core-utils/(.*)$': 'gatsby-core-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771
+    '^gatsby-plugin-utils/(.*)$': [
+      'gatsby-plugin-utils/dist/$1',
+      'gatsby-plugin-utils/$1',
+		]
 	},
 	testPathIgnorePatterns: [
-		'<rootDir>/node_modules/',
-		'<rootDir>/.cache/',
+		'node_modules',
+		'\\.cache/',
+		'<rootDir>.*/public',
 	],
 	transformIgnorePatterns: [
-		'<rootDir>/node_modules/(?!(gatsby)/)',
+		'node_modules/(?!(gatsby|gatsby-script)/)',
 	],
 	globals: {
 		__PATH_PREFIX__: '',
