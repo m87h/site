fa4ce71bcc8f52d648361839a895f6ccb67424d4
Martin Häger <martin.haeger@gmail.com>
2025-06-25 23:53:12 +0200

    fix(misc): Tabs to spaces 2: Electric Boogaloo

--- examples/wrapping-c-libraries-with-smart-pointers/sdl_example.c
+++ examples/wrapping-c-libraries-with-smart-pointers/sdl_example.c
@@ -1,15 +1,15 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* w = SDL_CreateWindow(
-		"example",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		0);
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* w = SDL_CreateWindow(
+    "example",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    640,
+    480,
+    0);
 
-	SDL_DestroyWindow(w);
-	SDL_Quit();
+  SDL_DestroyWindow(w);
+  SDL_Quit();
 }
