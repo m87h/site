dee78435c3ff26127d57cf962c2890c7af36665d
Martin Häger <martin.haeger@gmail.com>
2025-06-25 19:55:53 +0200

    fix(design): Relayout 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,20 @@
 import { graphql, Link } from 'gatsby';
-import { Grid, Segment, Header } from 'semantic-ui-react';
+import { Grid, GridColumn, Segment, Header } from 'semantic-ui-react';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
 const NotFoundPage = ({ data }) => (
-	<Grid style={{ height: '100vh' }} verticalAlign='middle' textAlign='center' columns={1}>
-		<Grid.Column stretched>
-			<Segment basic>
-				<GatsbyImage image={data.godzilla.childImageSharp.gatsbyImageData} alt='godzilla' />
-				<Header as='h1'>Godzilla has obliterated this page</Header>
-				<p>I'm truly sorry about that. Please <Link to='/'>go back</Link> and try one of the others.</p>
-				<p>(404)</p>
-			</Segment>
-		</Grid.Column>
-	</Grid>
+	<div style={{ position: 'absolute', top: 0, left: 0, height: '100vh', width: '100vw' }}>
+		<Grid style={{ height: '100%' }} verticalAlign='middle' textAlign='center' columns={1}>
+			<GridColumn>
+				<Segment basic>
+					<GatsbyImage image={data.godzilla.childImageSharp.gatsbyImageData} alt='godzilla' style={{ display: 'inline-block' }} />
+					<Header as='h1'>That page doesn't exist!</Header>
+					<p>Maybe it never did, or maybe it was destroyed by a fearsome beast.</p>
+					<p><Link to='/'>Go to the start page</Link></p>
+				</Segment>
+			</GridColumn>
+		</Grid>
+	</div>
 );
 
 export default NotFoundPage;
